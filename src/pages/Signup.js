3dae2220731240a22497fd0d8b010f9ee8e4e261
Robert-OP <popescu.robert.octavian@gmail.com>
2020-04-04 19:22:12 +0200

    add firebase, import material design fonts, implement signup/register

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,9 +1,75 @@
-import React, { Component } from "react";
+import React from 'react';
+import { Button, Grid, TextField } from '@material-ui/core';
 
-class Signup extends Component {
-  render() {
-    return <div>Signup</div>;
-  }
-}
+const Signup = () => {
+  const [form, setForm] = React.useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleChange = (prop) => (event) => {
+    setForm({ ...form, [prop]: event.target.value });
+  };
+
+  return (
+    <Grid container direction="row" justify="center" alignItems="center">
+      <form>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+          value={form.email}
+          onChange={handleChange('email')}
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          value={form.password}
+          onChange={handleChange('password')}
+        />
+
+        <Grid
+          container
+          direction="column"
+          justify="space-between"
+          alignItems="center"
+        >
+          <br />
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={() => {}}
+          >
+            Register
+          </Button>
+          <br />
+          <Button
+            fullWidth
+            variant="contained"
+            color="secondary"
+            onClick={() => {}}
+          >
+            Sign In
+          </Button>
+        </Grid>
+      </form>
+    </Grid>
+  );
+};
 
 export default Signup;
