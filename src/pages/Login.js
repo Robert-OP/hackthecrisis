a77cf773e8b1be997482ec3eb4f285ef442144b7
bertearazvan <bertea74@gmail.com>
2020-04-05 10:37:41 +0200

    added alert breadcrumbs

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Box, Button, TextField } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
 import firebase from 'components/Firebase';
+import Alert from '../components/Alert';
 
 const Login = () => {
   const [form, setForm] = React.useState({
     email: '',
     password: '',
+    error: '',
   });
   const [type, setType] = React.useState('user');
   const history = useHistory();
@@ -22,62 +25,87 @@ const Login = () => {
   const onLogin = async () => {
     try {
       const response = await firebase.login(form.email, form.password);
+      setForm({ error: '' });
       console.log(response);
       history.push('/dashboard');
     } catch (error) {
-      console.error(error);
+      // console.error(error);
+      setForm({ error: error, email: '', password: '' });
     }
   };
 
   return (
-    <Box id="dashboard-login">
-      <Box>
-        <h2>Log in</h2>
+    <Box>
+      <Grid container direction="row" justify="center" alignItems="center">
+        {form.error ? (
+          <Alert style={{ marginTop: '1rem' }} severity="error">
+            {form.error.message}
+          </Alert>
+        ) : (
+          ''
+        )}
+      </Grid>
+      <Grid container direction="row" justify="center" alignItems="center">
         <Box>
-          <form id="dashboardLoginForm">
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              value={form.email}
-              onChange={handleChange('email')}
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              value={form.password}
-              onChange={handleChange('password')}
-            />
+          <h2>Log in</h2>
+          <Box>
+            <form id="dashboardLoginForm">
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={form.email}
+                onChange={handleChange('email')}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={form.password}
+                onChange={handleChange('password')}
+              />
 
-            <Box>
-              <br />
-              <Button variant="contained" color="primary" onClick={onLogin}>
-                Log in
-              </Button>
-              <Button
-                variant="contained"
-                color="secondary"
-                onClick={() => onTypeChange()}
+              <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="center"
               >
-                {type === 'user' ? 'I am a doctor' : 'I am a user'}
-              </Button>
-            </Box>
-          </form>
+                <br />
+                <Button
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  onClick={onLogin}
+                >
+                  Log in
+                </Button>
+                <br />
+                <Button
+                  fullWidth
+                  variant="contained"
+                  color="secondary"
+                  onClick={() => onTypeChange()}
+                >
+                  {type === 'user' ? 'I am a doctor' : 'I am a user'}
+                </Button>
+              </Grid>
+            </form>
+          </Box>
         </Box>
-      </Box>
+      </Grid>
     </Box>
   );
 };
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Button, Grid, TextField } from '@material-ui/core';
+import { Box, Button, Grid, TextField } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
 import firebase from 'components/Firebase';
+import Alert from '../components/Alert';
 
 const Signup = () => {
   const [form, setForm] = React.useState({
     email: '',
     password: '',
     confirmPassword: '',
+    error: '',
   });
   const history = useHistory();
 
@@ -15,76 +17,122 @@ const Signup = () => {
     setForm({ ...form, [prop]: event.target.value });
   };
 
-  const onRegister = async () => {
-    try {
-      await firebase.register({
-        email: form.email,
-        password: form.password,
-        role: 'user',
+  const onRegister = async (e) => {
+    e.preventDefault();
+    if (form.password === form.confirmPassword) {
+      try {
+        const response = await firebase.register({
+          email: form.email,
+          password: form.password,
+          role: 'user',
+        });
+        setForm({ error: '' });
+        console.log(response);
+        history.push('/dashboard');
+      } catch (error) {
+        console.error(error);
+        setForm({
+          error: error.message,
+          email: '',
+          password: '',
+          confirmPassword: '',
+        });
+      }
+    } else {
+      setForm({
+        error: 'Passwords do not match',
+        email: '',
+        password: '',
+        confirmPassword: '',
       });
-      history.push('/dashboard');
-    } catch (error) {
-      console.error(error);
     }
   };
 
   return (
-    <Grid container direction="row" justify="center" alignItems="center">
-      <form>
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-          autoComplete="email"
-          autoFocus
-          value={form.email}
-          onChange={handleChange('email')}
-        />
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-          value={form.password}
-          onChange={handleChange('password')}
-        />
+    <Box>
+      <Grid container direction="row" justify="center" alignItems="center">
+        {form.error ? (
+          <Alert style={{ marginTop: '1rem' }} severity="error">
+            {form.error}
+          </Alert>
+        ) : (
+          ''
+        )}
+      </Grid>
+      <Grid container direction="row" justify="center" alignItems="center">
+        <form>
+          <h2>Sign up</h2>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={form.email}
+            onChange={handleChange('email')}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={form.password}
+            onChange={handleChange('password')}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="confirmPassword"
+            label="Confirm Password"
+            type="password"
+            id="confirmPassword"
+            autoComplete="current-password"
+            value={form.confirmPassword}
+            onChange={handleChange('confirmPassword')}
+          />
 
-        <Grid
-          container
-          direction="column"
-          justify="space-between"
-          alignItems="center"
-        >
-          <br />
-          <Button
-            fullWidth
-            variant="contained"
-            color="primary"
-            onClick={onRegister}
+          <Grid
+            container
+            direction="column"
+            justify="space-between"
+            alignItems="center"
           >
-            Register
-          </Button>
-          <br />
-          <Button
-            fullWidth
-            variant="contained"
-            color="secondary"
-            onClick={() => {}}
-          >
-            Sign In
-          </Button>
-        </Grid>
-      </form>
-    </Grid>
+            <br />
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              onClick={(e) => onRegister(e)}
+              type="submit"
+            >
+              Register
+            </Button>
+            <br />
+            <Button
+              fullWidth
+              variant="contained"
+              color="secondary"
+              onClick={() => {
+                history.push('/login');
+              }}
+            >
+              Sign In
+            </Button>
+          </Grid>
+        </form>
+      </Grid>
+    </Box>
   );
 };
 
